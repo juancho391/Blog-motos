0ed57889592981bca227b0a6ee2c8893091679cf
alejandro-grisales1 <alejandrogrisalesvelez@gmail.com>
2024-10-02 17:57:36 -0500

    estoy organizando la pantalla del celular quedo el titulo y las redes sociales listas

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        nav: "#939292e4",
+        redes: "#00000080",
+      },
+      margin: {
+        medioy: "50%",
+      },
+    },
+  },
+  plugins: [],
+};
